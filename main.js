ecd0f809ff666eeef158bad68f6f9d199d3d8062
Anna Topping <anna.topping@Anna-Toppings-MacBook-Air.local>
2020-04-27 15:40:12 -0400

    finished js

--- main.js
+++ main.js
@@ -22,16 +22,47 @@ function updateDrink(response) {
   $('span').html(response.data.drinks[0].strDrink)
   $('#cocktail-pic').html(`<img src="${response.data.drinks[0].strDrinkThumb}" alt="" style="width:200px;">`)
   $('#instructions').html(response.data.drinks[0].strInstructions)
-  $('#ingredient1').html(response.data.drinks[0].strIngredient1)
-  $('#ingredient2').html(response.data.drinks[0].strIngredient2)
-  $('#ingredient3').html(response.data.drinks[0].strIngredient3)
-  $('#ingredient4').html(response.data.drinks[0].strIngredient4)
-  $('#ingredient5').html(response.data.drinks[0].strIngredient5)
-  $('#ingredient6').html(response.data.drinks[0].strIngredient6)
-  $('#ingredient7').html(response.data.drinks[0].strIngredient7)
-  $('#ingredient8').html(response.data.drinks[0].strIngredient8)
-  $('#ingredient9').html(response.data.drinks[0].strIngredient9)
-  $('#ingredient10').html(response.data.drinks[0].strIngredient10)
+
+  const ingredients = [];
+  for (let i = 0; i < 15; i++) {
+    if (response.data.drinks[0][`strIngredient${i}`] != null) {
+      ingredients.push({
+        measure: response.data.drinks[0][`strMeasure${i}`],
+        ingredient: response.data.drinks[0][`strIngredient${i}`]
+      });
+    }
+  }
+
+  console.log(ingredients);
+
+  ingredients.forEach((ingredient, measure) => {
+    $('#ingredients-list').append(
+      `<li> ${ingredient.measure} ${ingredient.ingredient}</li>`)
+   // console.log(ingredient.measure)
+   // console.log(ingredient.ingredient)
+  })
+
+
+
+  // [{ingredient: string, measure: string}]
+
+  // loop over that array and append li to #ingredients-list for each item
+//
+// $('#ingredients-list').append();
+//
+//   $('#ingredient1').html(response.data.drinks[0].strIngredient1)
+//   $('#measure1').html(response.data.drinks[0].strMeasure1)
+//
+//
+//   $('#ingredient2').html(response.data.drinks[0].strIngredient2)
+//   $('#ingredient3').html(response.data.drinks[0].strIngredient3)
+//   $('#ingredient4').html(response.data.drinks[0].strIngredient4)
+//   $('#ingredient5').html(response.data.drinks[0].strIngredient5)
+//   $('#ingredient6').html(response.data.drinks[0].strIngredient6)
+//   $('#ingredient7').html(response.data.drinks[0].strIngredient7)
+//   $('#ingredient8').html(response.data.drinks[0].strIngredient8)
+//   $('#ingredient9').html(response.data.drinks[0].strIngredient9)
+//   $('#ingredient10').html(response.data.drinks[0].strIngredient10)
 }
 
 

2b42c0477f74fd3418859d2c340a8ebd9f252cdd
Anna Topping <anna.topping@Anna-Toppings-MacBook-Air.local>
2020-04-27 15:50:42 -0400

    oops

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ async function newCocktail() {
 
 ///
 function updateDrink(response) {
-  $('span').html(response.data.drinks[0].strDrink)
+  $('#cocktailName').html(response.data.drinks[0].strDrink)
   $('#cocktail-pic').html(`<img src="${response.data.drinks[0].strDrinkThumb}" alt="" style="width:200px;">`)
   $('#instructions').html(response.data.drinks[0].strInstructions)
 
@@ -44,28 +44,7 @@ function updateDrink(response) {
    // console.log(ingredient.measure)
    // console.log(ingredient.ingredient)
   })
-
-
-
-  // [{ingredient: string, measure: string}]
-
-  // loop over that array and append li to #ingredients-list for each item
-//
-// $('#ingredients-list').append();
-//
-//   $('#ingredient1').html(response.data.drinks[0].strIngredient1)
-//   $('#measure1').html(response.data.drinks[0].strMeasure1)
-//
-//
-//   $('#ingredient2').html(response.data.drinks[0].strIngredient2)
-//   $('#ingredient3').html(response.data.drinks[0].strIngredient3)
-//   $('#ingredient4').html(response.data.drinks[0].strIngredient4)
-//   $('#ingredient5').html(response.data.drinks[0].strIngredient5)
-//   $('#ingredient6').html(response.data.drinks[0].strIngredient6)
-//   $('#ingredient7').html(response.data.drinks[0].strIngredient7)
-//   $('#ingredient8').html(response.data.drinks[0].strIngredient8)
-//   $('#ingredient9').html(response.data.drinks[0].strIngredient9)
-//   $('#ingredient10').html(response.data.drinks[0].strIngredient10)
+  //
 }
 
 
